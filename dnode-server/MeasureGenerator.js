1290fddf72792faa98783206d1e3b29446081d2b
M-J-Jovanovic <bzflater@gmail.com>
2019-07-07 22:48:06 +0200

    new file:   dnode-server/.idea/vcs.xml 	modified:   dnode-server/.idea/workspace.xml 	modified:   dnode-server/MeasureGenerator.js 	new file:   dnode-server/node-wrapper.js 	modified:   dnode-server/node_modules/dnode/index.js 	modified:   dnode-server/server.js 	modified:   neuralbeats/application/controllers/generator_controller.php 	new file:   neuralbeats/inc/jasmid/LICENSE 	new file:   neuralbeats/inc/jasmid/midifile.js 	new file:   neuralbeats/inc/jasmid/replayer.js 	new file:   neuralbeats/inc/jasmid/stream.js 	new file:   neuralbeats/inc/shim/Base64.js 	new file:   neuralbeats/inc/shim/Base64binary.js 	new file:   neuralbeats/inc/shim/WebAudioAPI.js 	new file:   neuralbeats/inc/shim/WebMIDIAPI.js 	new file:   neuralbeats/inc/tuna/README.md 	new file:   neuralbeats/inc/tuna/impulses/Sweetspot1M.wav 	new file:   neuralbeats/inc/tuna/impulses/impulse_guitar.wav 	new file:   neuralbeats/inc/tuna/impulses/impulse_rev.wav 	new file:   neuralbeats/inc/tuna/impulses/ir_rev_short.wav 	new file:   neuralbeats/inc/tuna/tuna.js 	new file:   neuralbeats/js/midi/audioDetect.js 	new file:   neuralbeats/js/midi/gm.js 	new file:   neuralbeats/js/midi/loader.js 	new file:   neuralbeats/js/midi/player.js 	new file:   neuralbeats/js/midi/plugin.audiotag.js 	new file:   neuralbeats/js/midi/plugin.webaudio.js 	new file:   neuralbeats/js/midi/plugin.webmidi.js 	new file:   neuralbeats/js/midi/synesthesia.js 	new file:   neuralbeats/js/util/dom_request_script.js 	new file:   neuralbeats/js/util/dom_request_xhr.js 	new file:   neuralbeats/js/util/jquery-3.4.1.js

--- dnode-server/MeasureGenerator.js
+++ dnode-server/MeasureGenerator.js
@@ -11,11 +11,11 @@ let LSTM = null;
 let PCA_decoder = null;
 let _LSTM_path = null;
 let _PCA_decoder_path = null;
-let LSTM_data = null;
-let parameters = Array(40).fill(0.5);
-let used_parameters=Array(40).fill(0.5);
+let _LSTM_data = null;
+let _parameters = Array(40).fill(0.5);
+let _used_parameters=Array(40).fill(0.5);
 
-module.exports = internal.MeasureGenerator = class 
+module.exports = internal.MeasureGenerator = class {
 	/*
 	konstruktor, cuva puteve na mozgove
 	*/
@@ -30,7 +30,7 @@ module.exports = internal.MeasureGenerator = class
         if (id > 39 || id < 0) {
             throw "Out of bounds!";
         } else {
-            parameters[id] = value;
+            _parameters[id] = value;
         }
     }
 	/*
@@ -39,20 +39,35 @@ module.exports = internal.MeasureGenerator = class
     async generate() {
         if (null == LSTM) LSTM = await tfjs.loadLayersModel(_LSTM_path);
         if (null == PCA_decoder) PCA_decoder = await tfjs.loadLayersModel(_PCA_decoder_path);
-        if (JSON.stringify(parameters)!=JSON.stringify(used_parameters) || null == LSTM_data) {
-            used_parameters=[...parameters];
-            let decoded = PCA_decoder.predict(tfjs.tensor2d(parameters, [1, 40]));
+        if (JSON.stringify(_parameters)!=JSON.stringify(_used_parameters) || null == _LSTM_data) {
+            _used_parameters=[..._parameters];
+            let decoded = PCA_decoder.predict(tfjs.tensor2d(_parameters, [1, 40]));
             decoded = await decoded.data();
             decoded = Array.prototype.slice.call(decoded);
-            LSTM_data = math.reshape(decoded, [1, 8, 512]);
-            console.log("Updated parameters!");
+            _LSTM_data = math.reshape(decoded, [1, 8, 512]);
+            //console.log("Updated parameters!");
         }
-        let new_elem = await LSTM.predict(tfjs.tensor3d(LSTM_data)).data();
+        let new_elem = await LSTM.predict(tfjs.tensor3d(_LSTM_data)).data();
         new_elem = Array.prototype.slice.call(new_elem);
-        LSTM_data[0] = LSTM_data[0].slice(1, 8);
-        LSTM_data[0].push(new_elem);
+        _LSTM_data[0] = _LSTM_data[0].slice(1, 8);
+        _LSTM_data[0].push(new_elem);
         return new_elem;
     }
+	toJSON(){
+		return{
+			LSTM_path: _LSTM_path,
+			PCA_decoder_path: _PCA_decoder_path,
+			parameters: _parameters,
+			used_parameters: _used_parameters,
+			LSTM_data: _LSTM_data
+		};
+	}
+	static fromJSON(obj){
+		_parameters=obj.parameters;
+		_used_parameters=obj.parameters;
+		_LSTM_data=obj.LSTM_data;
+		return this(obj.LSTM_path, obj.PCA_decoder_path);
+	}
 }
 
 /*let generator = new internal.MeasureGenerator("https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/LSTM/model.json", "https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/PCADecoder/model.json");
--- dnode-server/node-wrapper.js
+++ dnode-server/node-wrapper.js
@@ -1,15 +1,19 @@
 //2015/0231 Marija Jovanovic
 /*
-DNode server 0.3 - koristi se kao omotac funkcionalnosti neuralnih mreza za pozive iz php
+omotac funkcionalnosti generatora, pozivani direktno iz PHP, verzija 1.0
 */
 'use strict'
 
-const dnode=require("./node_modules/dnode");
 const Generator=require("./MeasureGenerator");
 const Decoder=require("./MeasureAutoencoder");
-const net=require("net");
-let generators={};
-let sid=0;
+const args=process.argv.slice(2);
+if(args[0]=="open"){
+	const generator=new Generator("https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/LSTM/model.json", "https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/PCAdecoder/model.json");
+	generator.generate().then(function (value) {
+		process.stdout.write(JSON.stringify(generator.toJSON()));
+	});
+	//process.stdout.write(JSON.stringify(generator.toJSON()));
+}
 const decoder=new Decoder("https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/decoder/model.json");
 // const server=dnode({
 	// /*
@@ -47,47 +51,4 @@ const decoder=new Decoder("https://raw.githubusercontent.com/M-J-Jovanovic/Neura
         // delete generators[session_id];
     // }
 // });
-var server=net.createServer(function(c){
-	var d=dnode({
-		/*
-		kreira instancu generator muzike za tekucu sesiju i vraca id sesije
-		*/
-		open_session: function(callback){
-			process.stdout.write("Attempting to open session.\n");
-			generators[sid]=new Generator("https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/LSTM/model.json", "https://raw.githubusercontent.com/M-J-Jovanovic/Neural-Beats/master/dnode-server/PCADecoder/model.json");
-			process.stdout.write("Opened session number "+str(sid)+".\n");
-			callback(sid++);
-		},
-		/*
-		menja neki parametar
-		*/
-		set_parameter: function(id, value, session_id){
-			if(!Object.keys(generators).includes(session_id)) return;
-			generators[session_id].set_parameter(id%40,value/100)
-		},
-		/*
-		generise muziku i prevodi nju u citljivi format, vraca niz u tom citljivom formatu
-		*/
-		generate: function(session_id,callback){
-			if(!Object.keys(generators).includes(session_id)) return;
-			generators[session_id].generate().then(function(value){
-				decoder.decode(value).then(function(decoded_value){
-					callback(JSON.stringify(decoded_value));
-				});
-			});
-		},
-		/*
-		brise instancu generatora muzike za ovu sesiju
-		*/
-		close_session: function(session_id){
-			if(!Object.keys(generators).includes(session_id)) return;
-			delete generators[session_id];
-		}
-	});
-	c.pipe(d).pipe(c);
-});
-server.on("error",function(err){console.log(err);});
-process.on('uncaughtException', function(err) {
-    console.error(' Caught exception: ', err.stack, err);
-});
-server.listen(7070);
+
