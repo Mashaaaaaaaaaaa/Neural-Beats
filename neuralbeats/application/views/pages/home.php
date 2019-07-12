ba015505ff95fac97347a003ef6e21686d67638a
jankelaa <48515964+jankelaa@users.noreply.github.com>
2019-07-13 00:38:41 +0200

    view: home.php

--- neuralbeats/application/views/pages/home.php
+++ neuralbeats/application/views/pages/home.php
@@ -5,5 +5,11 @@
     <div>&nbsp;</div>
     <img src="<?php echo base_url('images/neural_beats.png'); ?>" class="home">
     <div>&nbsp</div>
-    <span class="font"><a href="<?php echo base_url(); ?>generator" class="yellow">Otvorite generator</a></span>
+    <span class="font"><a href="<?php
+    if(!$this->session->has_userdata('korisnik')){ 
+        echo base_url().'generator';
+        
+    }
+        else echo base_url().'korisnik/view/generator';
+    ?>" class="yellow">Otvorite generator</a></span>
 </div>
